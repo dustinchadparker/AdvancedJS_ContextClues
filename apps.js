7827cb324da3567a17fe82bd5f33bf52f92b8696
Dustin Chad Parker <dustin.chad.parker@gmail.com>
2019-02-04 20:51:12 -0600

    Finishing touches. Styles, .md change

--- apps.js
+++ apps.js
@@ -1,32 +1,66 @@
-$( document ).ready(function() {
-    
-    var accusationNum = 1;
-    var friends = ["Marky", "Ricky", "Danny", "Terry", "Mikey"];
-    var locations = ["car", "bathroom", "alley", "den", "closet", "garbage", "washing machine", "bed", "attic", "shower"]
-    var objects = ["spoon", "fork", "keyboard", "bottle", "monitor", 
-                   "television", "mouse", "headphones", "earbuds", "showerhead",
-                   "ring", "ornament", "shovel", "carkeys", "pillow",
-                    "stuffed animal", "knife", "axe", "sword", "bat"];
+//Waits until document has loaded
 
-    for(accusationNum; accusationNum < 100; accusationNum++) {
+$(document).ready(function() {
+  var accusationNum = 1; //increment for for-loop
+  //declare and instantiate variables
+  var friends = ["Marky", "Ricky", "Danny", "Terry", "Mikey"];
+  var locations = [
+    "car",
+    "bathroom",
+    "alley",
+    "den",
+    "closet",
+    "garbage",
+    "washing machine",
+    "bed",
+    "attic",
+    "shower"
+  ];
+  var objects = [
+    "spoon",
+    "fork",
+    "keyboard",
+    "bottle",
+    "monitor",
+    "television",
+    "mouse",
+    "headphones",
+    "earbuds",
+    "showerhead",
+    "ring",
+    "ornament",
+    "shovel",
+    "carkeys",
+    "pillow",
+    "stuffed animal",
+    "knife",
+    "axe",
+    "sword",
+    "bat"
+  ];
 
-        $(document.body).append('<h3 id=' + accusationNum + '>' + 'Accusation ' + accusationNum + ':</h3>');
+  //loops through, creating 99 accusations on the page
+  for (accusationNum; accusationNum < 100; accusationNum++) {
+    $(document.body).append(
+      "<h3 id=" + accusationNum + ">" + "Accusation " + accusationNum + ":</h3>"
+    );
+  }
 
-    }
+  //button click that gets a random number, which
+  //calls a random friend/object/location
+  $("h3").click(function() {
+    var i = Math.floor(Math.random() * 5);
+    var friendChosen = friends[i];
+    i = Math.floor(Math.random() * 10);
+    var locationChosen = locations[i];
+    i = Math.floor(Math.random() * 20);
+    var objectChosen = objects[i];
 
-    $('h3').click(function() {
-
-        var i = Math.floor(Math.random() * 5);
-        var friendChosen = friends[i];
-        var i = Math.floor(Math.random() * 10);
-        var locationChosen = locations[i];
-        var i = Math.floor(Math.random() * 20);
-        var objectChosen = objects[i];
-
-
-        alert("Accusation " + this.id + ": I accuse " + friendChosen + ", with the "
-                + objectChosen + " in the " + locationChosen + "!");
-      
-    });
-
+    //an alert displaying accusation
+    alert(
+      `Accusation ${
+        this.id
+      }: I accuse ${friendChosen}, with the ${objectChosen} in the ${locationChosen}!`
+    );
+  });
 });
